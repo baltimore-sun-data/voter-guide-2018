fb38bc79869eeddb2fc491b30e84bb494d88703f
Carl Johnson <me@carlmjohnson.net>
2018-02-07 17:05:00 -0500

    Fix formatting with Prettier

--- static/scripts/javascript.js
+++ static/scripts/javascript.js
@@ -1,35 +1,27 @@
 function filter_party(party) {
   if (party == "democrats") {
-
-        $("#democrats").show();
-        $("#republicans").hide();
-        $("#greens").hide();
-        $('#choosePartyText').empty();
-        $('#choosePartyText').text('Democrats');
-
-    }  else if (party == "republicans") {
-
-        $("#democrats").hide();
-        $("#republicans").show();
-        $("#greens").hide();
-        $('#choosePartyText').empty();
-        $('#choosePartyText').text('Republicans');
-
-  }  else if (party == "greens") {
-
-        $("#democrats").hide();
-        $("#republicans").hide();
-        $("#greens").show();
-        $('#choosePartyText').empty();
-        $('#choosePartyText').text('Greens');
-
+    $("#democrats").show();
+    $("#republicans").hide();
+    $("#greens").hide();
+    $("#choosePartyText").empty();
+    $("#choosePartyText").text("Democrats");
+  } else if (party == "republicans") {
+    $("#democrats").hide();
+    $("#republicans").show();
+    $("#greens").hide();
+    $("#choosePartyText").empty();
+    $("#choosePartyText").text("Republicans");
+  } else if (party == "greens") {
+    $("#democrats").hide();
+    $("#republicans").hide();
+    $("#greens").show();
+    $("#choosePartyText").empty();
+    $("#choosePartyText").text("Greens");
   } else if (party == "all") {
-
-        $("#democrats").show();
-        $("#republicans").show();
-        $('#choosePartyText').empty();
-        $('#choosePartyText').text('Choose Party');
-
+    $("#democrats").show();
+    $("#republicans").show();
+    $("#choosePartyText").empty();
+    $("#choosePartyText").text("Choose Party");
   }
 }
 
