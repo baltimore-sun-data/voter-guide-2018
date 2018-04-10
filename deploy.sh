274eb833715fc91ba6922e9db9a99aacf00ccf04
Carl Johnson <me@carlmjohnson.net>
2018-04-10 11:25:22 -0400

    Deployer: Add AWS region

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -eux -o pipefail
 
 echo "Deploying site to S3"
-s3deploy -config s3deploy.yaml -source public -bucket elections2018-news-baltimoresun-com -v
+s3deploy -config s3deploy.yaml -source public -bucket elections2018-news-baltimoresun-com -region us-east-1 -v
 
 echo "Clearing CloudFront cache"
 boreas -dist E2AXRGC2L5V6PH
