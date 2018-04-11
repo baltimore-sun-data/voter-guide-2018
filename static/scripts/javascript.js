de7c59a8539432000b489b10e88584ad34235e3b
Carl Johnson <me@carlmjohnson.net>
2018-04-11 17:07:17 -0400

    Simply JS for filtering

--- static/scripts/javascript.js
+++ static/scripts/javascript.js
@@ -329,38 +329,31 @@ var app = {
 
   candidate_table_filter: function() {
     // Source: https://codepen.io/chriscoyier/pen/tIuBL
-    var LightTableFilter = (function(Arr) {
-      var _input;
+    var Arr = Array.prototype;
+    var _input;
 
-      function _onInputEvent(e) {
-        _input = e.target;
-        var tables = document.getElementsByClassName(
-          _input.getAttribute("data-table")
-        );
-        Arr.forEach.call(tables, function(table) {
-          Arr.forEach.call(table.tBodies, function(tbody) {
-            Arr.forEach.call(tbody.rows, _filter);
-          });
+    function _onInputEvent(e) {
+      _input = e.target;
+      var tables = document.getElementsByClassName(
+        _input.getAttribute("data-table")
+      );
+      Arr.forEach.call(tables, function(table) {
+        Arr.forEach.call(table.tBodies, function(tbody) {
+          Arr.forEach.call(tbody.rows, _filter);
         });
-      }
+      });
+    }
 
-      function _filter(row) {
-        var text = row.textContent.toLowerCase();
-        var val = _input.value.toLowerCase();
-        row.style.display = text.indexOf(val) === -1 ? "none" : "table-row";
-      }
+    function _filter(row) {
+      var text = row.textContent.toLowerCase();
+      var val = _input.value.toLowerCase();
+      row.style.display = text.indexOf(val) === -1 ? "none" : "table-row";
+    }
 
-      return {
-        init: function() {
-          var inputs = document.getElementsByClassName("light-table-filter");
-          Arr.forEach.call(inputs, function(input) {
-            input.oninput = _onInputEvent;
-          });
-        }
-      };
-    })(Array.prototype);
-
-    LightTableFilter.init();
+    var inputs = document.getElementsByClassName("light-table-filter");
+    Arr.forEach.call(inputs, function(input) {
+      input.oninput = _onInputEvent;
+    });
   }
 };
 
