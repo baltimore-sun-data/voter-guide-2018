4fc57a1338a8abfb4f8224d4326b67ed3ebbf7b5
Carl Johnson <me@carlmjohnson.net>
2018-05-14 16:55:05 -0400

    Robocopy: Basic uploading by interval

--- cmd/robocopy/robocopy.go
+++ cmd/robocopy/robocopy.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	humanize "github.com/dustin/go-humanize"
 )
 
@@ -38,6 +43,10 @@ type Config struct {
 	ResultsLocation  string
 	OutputDir        string
 	TemplateGlob     string
+	Region           string
+	Bucket           string
+	Path             string
+	PollInterval     time.Duration
 }
 
 func FromArgs(args []string) *Config {
@@ -49,6 +58,10 @@ func FromArgs(args []string) *Config {
 	fl.StringVar(&conf.ResultsLocation, "results-src", results18url, "url or filename for results")
 	fl.StringVar(&conf.OutputDir, "output-dir", "dist/results/contests", "directory to save into")
 	fl.StringVar(&conf.TemplateGlob, "template-glob", "layouts-robocopy/*.html", "pattern to look for templates with")
+	fl.StringVar(&conf.Region, "region", "us-east-1", "Amazon region for S3")
+	fl.StringVar(&conf.Bucket, "bucket", "elections2018-news-baltimoresun-com", "Amazon S3 bucket")
+	fl.StringVar(&conf.Path, "path", "/results/contests/", "Amazon S3 destination path")
+	fl.DurationVar(&conf.PollInterval, "poll-interval", 30*time.Second, "time between refreshing S3")
 	fl.Usage = func() {
 		fmt.Fprintf(os.Stderr,
 			`robocopy
@@ -65,21 +78,17 @@ Usage of robocopy:
 }
 
 func (c *Config) Exec() error {
-	if !c.Local {
-		return fmt.Errorf("not implemented")
-	}
-
-	var funcMap = map[string]interface{}{
-		"commas": func(i int) string { return humanize.Comma(int64(i)) },
-	}
-	t, err := template.New("").Funcs(funcMap).ParseGlob(c.TemplateGlob)
+	t, err := c.template()
 	if err != nil {
-		return fmt.Errorf("could not load templates from %s: %v", c.TemplateGlob, err)
+		return err
 	}
-
 	m, err := MetadataFrom(c.MetadataLocation)
 	if err != nil {
 		return err
+	}
+
+	if !c.Local {
+		return c.Remote(m, t)
 	}
 
 	if c.CreateResults {
@@ -128,4 +137,77 @@ func (c *Config) createFile(t *template.Template, tplname, filename string, data
 	defer deferClose(&err, f.Close)
 
 	return t.ExecuteTemplate(f, tplname, data)
+}
+
+func (c *Config) Remote(m *Metadata, t *template.Template) error {
+	s, err := session.NewSession(&aws.Config{
+		Region: aws.String(c.Region),
+	})
+	if err != nil {
+		return fmt.Errorf("bad AWS credentials: %v", err)
+	}
+	var cl = client{
+		uploader: s3manager.NewUploader(s),
+		cachecontrol: aws.String(
+			fmt.Sprintf("public, max-age=%.0f", c.PollInterval.Seconds()),
+		),
+		template: t,
+	}
+
+	for range time.Tick(c.PollInterval) {
+		log.Print("getting results")
+		r, err := ResultsContainerFrom(c.ResultsLocation)
+		if err != nil {
+			log.Printf("results error: %v", err)
+			continue
+		}
+
+		cr := MapContestResults(m, r)
+		for cid, rp := range cr {
+			filename := fmt.Sprintf("%d.html", cid)
+			err = c.uploadFile(cl, filename, rp)
+			if err != nil {
+				return err
+			}
+		}
+		log.Print("finished uploading")
+	}
+	panic("unreachable")
+}
+
+var funcMap = map[string]interface{}{
+	"commas": func(i int) string { return humanize.Comma(int64(i)) },
+}
+
+func (c *Config) template() (*template.Template, error) {
+	t, err := template.New("").Funcs(funcMap).ParseGlob(c.TemplateGlob)
+	if err != nil {
+		return nil, fmt.Errorf("could not load templates from %s: %v", c.TemplateGlob, err)
+	}
+	return t, err
+}
+
+type client struct {
+	uploader     *s3manager.Uploader
+	template     *template.Template
+	cachecontrol *string
+}
+
+func (c *Config) uploadFile(cl client, filename string, data interface{}) error {
+	var buf bytes.Buffer
+	err := cl.template.ExecuteTemplate(&buf, "contest.html", data)
+	if err != nil {
+		return fmt.Errorf("error executing template: %v", err)
+	}
+
+	_, err = cl.uploader.Upload(&s3manager.UploadInput{
+		Bucket:       aws.String(c.Bucket),
+		Key:          aws.String(c.Path + filename),
+		CacheControl: cl.cachecontrol,
+		Body:         &buf,
+	})
+	if err != nil {
+		log.Printf("error uploading %s: %v", filename, err)
+	}
+	return nil
 }
