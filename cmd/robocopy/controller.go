dd588d0851674c7b36cd9d1ce6f869c32779697e
Carl Johnson <me@carlmjohnson.net>
2018-11-07 13:36:35 -0500

    Robocopy: Sort by votes again

--- cmd/robocopy/controller.go
+++ cmd/robocopy/controller.go
@@ -224,14 +224,13 @@ func MapContestResults(m *Metadata, rc *ResultsContainer) map[ContestID]*Result 
 		for i, opt := range result.Options {
 			result.SubResultOptions[i] = opt.Text
 		}
-		// TODO: Turn back on sorting by vote after election night
-		// // Now sort in vote order
-		// sort.Slice(result.Options, func(i, j int) bool {
-		// 	if result.Options[i].TotalVotes == result.Options[j].TotalVotes {
-		// 		return result.Options[i].Order < result.Options[j].Order
-		// 	}
-		// 	return result.Options[i].TotalVotes > result.Options[j].TotalVotes
-		// })
+		// Now sort in vote order
+		sort.Slice(result.Options, func(i, j int) bool {
+			if result.Options[i].TotalVotes == result.Options[j].TotalVotes {
+				return result.Options[i].Order < result.Options[j].Order
+			}
+			return result.Options[i].TotalVotes > result.Options[j].TotalVotes
+		})
 		sort.Slice(result.SubResults, func(i, j int) bool {
 			if result.SubResults[i].District == result.SubResults[j].District {
 				return result.SubResults[i].Jurisdiction < result.SubResults[j].Jurisdiction
